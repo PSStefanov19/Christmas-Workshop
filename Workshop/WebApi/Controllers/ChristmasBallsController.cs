793529579b0dea9a8cf6e41e5f4721d4bc50e04e
PSStefanov19 <PSStefanov19@codingburgas.bg>
2023-12-19 10:14:59 +0200

    Create basic Get request

---
+++ Workshop/WebApi/Controllers/ChristmasBallsController.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class ChristmasBallsController : Controller
+    {
+        [HttpGet("get")]
+        public List<ChristmasBall> Get() 
+        {
+            return ChristmasBallsRepository.GetAllBalls();
+        }
+    }
+}
