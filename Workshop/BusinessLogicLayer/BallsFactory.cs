db3b8d3c8ea834b360ffa116d6c9daea8a2b41c9
Борис Добрев Симеонов <BDSimeonov19@codingburgas.bg>
2023-12-19 10:32:20 +0200

    Add factory

---
+++ Workshop/BusinessLogicLayer/BallsFactory.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Models;
+
+namespace BusinessLogicLayer
+{
+    public class BallsFactory
+    {
+        ChristmasBall CreateBall()
+        {
+            ChristmasBall ball = new();
+
+            return ball;      
+        }
+    }
+}
