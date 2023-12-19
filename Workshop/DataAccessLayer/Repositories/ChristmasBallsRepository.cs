ceda76349e955477e131692198a04fd6fc1f4937
Борис Добрев Симеонов <BDSimeonov19@codingburgas.bg>
2023-12-19 09:55:24 +0200

    Add repositories

---
+++ Workshop/DataAccessLayer/Repositories/ChristmasBallsRepository.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer.Repositories
+{
+    public class ChristmasBallsRepository
+    {
+        public static void CreateBall(ChristmasBall ball)
+        {
+            using BallzContext context = new();
+            context.Add(ball);
+            context.SaveChanges();
+        }
+
+        public static List<ChristmasBall> GetAllBalls()
+        {
+            using BallzContext context = new();
+            return context.ChristmasBalls.ToList();
+        }
+    }
+}
